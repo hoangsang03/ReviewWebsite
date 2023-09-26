43ecdf6cfbcb4df5d15c78084acf6778816ca02a
hoangsang03 <nguyenhoangsangtth2@gmail.com>
2023-09-26 23:47:42 +0700

    add Token Validation

---
+++ ReviewWebsite.Api/Controllers/DinnerController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReviewWebsite.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class DinnerController : ApiController
+    {
+        [HttpGet]
+        public IActionResult ListDinner()
+        {
+            return Ok(Array.Empty<string>());
+        }
+    }
+}
